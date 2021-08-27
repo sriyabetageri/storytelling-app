ededece6d8775a5e417fb64444b913cf29320c6e
sriyabetageri <75461862+sriyabetageri@users.noreply.github.com>
2021-08-26 18:53:35 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+
+  // Your web app's Firebase configuration
+  export const firebaseConfig = {
+    apiKey: "AIzaSyCCWRo_mGRgA5NIOuOtLsVVoPsAeP_UdAY",
+    authDomain: "storytellingapp-d9cc1.firebaseapp.com",
+    projectId: "storytellingapp-d9cc1",
+    storageBucket: "storytellingapp-d9cc1.appspot.com",
+    messagingSenderId: "362194875336",
+    appId: "1:362194875336:web:f2e01e084e1ab57fb546fd"
+  };
+  // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);
